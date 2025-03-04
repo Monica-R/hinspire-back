32ee096f990d7198400ee64f670435084e9af3fe
Monica-R <moniroka_339@hotmail.com>
2025-03-04 19:13:03 +0000

    feat: set routers and handlinf errors added

--- src/app.js
+++ src/app.js
@@ -3,14 +3,27 @@
 
 import express from 'express';
 import { config } from './config/index.js';
+import { handlingErrors } from './errors/index.js';
+import authRoutes from './routes/auth.routes.js';
+import profileRoute from './routes/profile.routes.js';
+import adminRoutes from './routes/admin.routes.js';
 
 const app = express();
 config.applyMiddlewares(app);
 
-app.use("/", (req, res) => {
+
+// 👇 Start handling routes here
+
+app.use("/", () => {
     console.info('ESTAMOS EN SERVER!!');
-    res.send('<h1>hola desde el SERVIDOR!</h1>');
-})
+});
+
+app.use("/auth", authRoutes);
+app.use("/user", profileRoute);
+app.use("/admin", adminRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+handlingErrors(app);
 
 
 export default app;
