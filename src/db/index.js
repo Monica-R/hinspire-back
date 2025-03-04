f51cd239b4da9259da03c801b1f573e6859c9f39
Monica-R <moniroka_339@hotmail.com>
2025-03-04 19:42:38 +0000

    feat: connection with mongodb included.

--- src/db/index.js
+++ src/db/index.js
@@ -1,1 +1,15 @@
 // Aquí establecemos la conexión con la BBDD, en esta ocasión, utilizaremos Mongo DB con mongoose
+import mongoose from "mongoose";
+
+// Conexión a la BBDD
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/hinspire-db";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    const dbName = mongoose.connection.name;
+    console.log(`Name database: ${dbName}`);
+  } catch (error) {
+    console.error("Error connecting to database", error);
+  }
+};
