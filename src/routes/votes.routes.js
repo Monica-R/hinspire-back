8c2f2ff4f98a837b676a32d0ad411312c4a1b2db
Monica-R <moniroka_339@hotmail.com>
2025-03-07 17:48:17 +0000

    fix: / missing in delete route

--- src/routes/votes.routes.js
+++ src/routes/votes.routes.js
@@ -5,6 +5,6 @@ import { isAuthenticated } from '../middlewares/jwt.middleware.js';
 const router = express();
 
 router.post("/vote/:fragmentId", isAuthenticated, addVote);
-router.delete("vote/:fragmentId", isAuthenticated, deleteVote);
+router.delete("/vote/:fragmentId", isAuthenticated, deleteVote);
 
 export default router;
