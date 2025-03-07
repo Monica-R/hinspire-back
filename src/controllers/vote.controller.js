0fc1315a38c2e211867a74a4e81c36447c2d51bd
Monica-R <moniroka_339@hotmail.com>
2025-03-07 17:34:57 +0000

    feat: vote controller.js added

---
+++ src/controllers/vote.controller.js
@@ -0,0 +1,50 @@
+import Vote from '../models/Vote.model.js';
+import Fragment from '../models/Fragment.model.js';
+
+export const addVote = async (req, res, next) => {
+    try {
+        const userId = req.payload._id;
+        const fragmentId = req.params.fragmentId;
+
+        // Comprobamos si el usuario ha votado
+        // Lo buscamos con findOne
+        const existingVote = await Vote.findOne({ user: userId, fragment: fragmentId });
+        if (existingVote) {
+            res.status(400).send("You are already vote for this fragment.");
+            return;
+        }
+
+        // Creamos el voto
+        const newVote = await Vote.create( { user: userId, fragment: fragmentId });
+
+        // Incrementamos el nº de votos en el model fragment
+        await Fragment.findByIdAndUpdate(fragmentId, { $inc: { votes: 1 } });
+        res.status(200).json({ message: "You are voted successfully.",  vote: newVote});
+
+    } catch (error) {
+        next(error);   
+    }
+}
+
+export const deleteVote = async (req, res, next) => {
+    try {
+        const userId = req.payload._id;
+        const fragmentId = req.params.fragmentId;
+
+        // Comprobamos si el voto existe
+        const voto = Vote.findOne({ user: userId, fragment: fragmentId });
+        if (!voto) {
+            res.status(400).send("You are not voted for this fragment.");
+            return;
+        }
+
+        // Eliminamos el voto
+        await Vote.findByIdAndDelete(vote._id);
+
+        // Reducimos la cantidad de votos a una
+        await Fragment.findByIdAndUpdate(fragmentId, { $inc: { votes: -1 } });
+        res.status(200).json({ message: "Vote removed successfully." });
+    } catch (error) {
+        next(error);
+    }
+}
