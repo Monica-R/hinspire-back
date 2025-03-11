6426cf00ce5e8b507cc9a8adda5c3f7d0a8f1802
Monica-R <moniroka_339@hotmail.com>
2025-03-11 17:07:27 +0000

    feat: add route to retrieve user's votes

--- src/routes/votes.routes.js
+++ src/routes/votes.routes.js
@@ -6,5 +6,6 @@ const router = express();
 
 router.post("/vote/:fragmentId", isAuthenticated, addVote);
 router.delete("/vote/:fragmentId", isAuthenticated, deleteVote);
+router.get("/myvotes", isAuthenticated, getUserVotes);
 
 export default router;
