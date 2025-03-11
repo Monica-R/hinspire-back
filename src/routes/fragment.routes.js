ede3ff794ac57c34c3d31cb5a687b2837f09632d
Monica-R <moniroka_339@hotmail.com>
2025-03-11 12:29:47 +0000

    fix: add authentication middleware to fragment routes

--- src/routes/fragment.routes.js
+++ src/routes/fragment.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { addFragment, getFragmentsByStory, editFragment, deleteFragment } from '../controllers/fragment.controller.js';
+import { isAuthenticated } from '../middlewares/jwt.middleware.js';
 
 const router = express();
 
 //router.get("/", getFragments);
 router.get("/story/:id", getFragmentsByStory);
-router.post("/", addFragment);
-router.put("/:id", editFragment);
-router.delete("/:id", deleteFragment);
+router.post("/", isAuthenticated, addFragment);
+router.put("/:id", isAuthenticated, editFragment);
+router.delete("/:id", isAuthenticated, deleteFragment);
 
 export default router;
