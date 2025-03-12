e1843fc89de686891dd0d604f320212574400add
Monica-R <moniroka_339@hotmail.com>
2025-03-12 12:54:13 +0000

    feat: add route to accept fragment by story and fragment ID

--- src/routes/fragment.routes.js
+++ src/routes/fragment.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addFragment, getFragmentsByStory, editFragment, deleteFragment } from '../controllers/fragment.controller.js';
+import { addFragment, getFragmentsByStory, editFragment, deleteFragment, acceptFragment } from '../controllers/fragment.controller.js';
 import { isAuthenticated } from '../middlewares/jwt.middleware.js';
 
 const router = express();
 
 //router.get("/", getFragments);
+router.put("/:storyId/confirm/:fragmentId", acceptFragment);
 router.get("/story/:id", getFragmentsByStory);
 router.post("/:id", isAuthenticated, addFragment);
 router.put("/:id", isAuthenticated, editFragment);
